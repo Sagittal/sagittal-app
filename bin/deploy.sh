7a605a495531045310f2287f40e8598a513fb2a7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-07 09:23:55 -0800

    submodule sagittal.github.io and add a script to deploy it

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+npm version patch
+npm run build
+CURRENT_VERSION=$(< package.json grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[:space:]')
+pushd dist || exit
+  git add .
+  git commit -m "${CURRENT_VERSION}"
+  git push
+popd || exit
