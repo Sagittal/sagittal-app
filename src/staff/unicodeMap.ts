9cca2b676a6ddd11157aa3b9efa2d30a5c8ff526
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-10 09:06:17 -0800

    v1.0.9: fix sp14 and sp15

--- src/staff/unicodeMap.ts
+++ src/staff/unicodeMap.ts
@@ -1,22 +1,24 @@
 import {ACCIDENTALS} from "./accidentals"
 import {Code, Uni} from "./types"
 
-const sp1 = " " as Uni      // U+200A           HAIR SPACE
-const sp2 = " " as Uni      // U+2009           THIN SPACE
+const sp1 = " " as Uni      // U+200A                   HAIR SPACE
+const sp2 = " " as Uni      // U+2009                   THIN SPACE
 const sp3 = "  " as Uni     // U+2009 U+200A
-const sp4 = " " as Uni      // U+2005           FOUR-PER-EM SPACE
+const sp4 = " " as Uni      // U+2005                   FOUR-PER-EM SPACE
 const sp5 = "  " as Uni     // U+2005 U+200A
-const sp6 = " " as Uni      // U+2004           THREE-PER-EM SPACE
+const sp6 = " " as Uni      // U+2004                   THREE-PER-EM SPACE
 const sp7 = "  " as Uni     // U+2004 U+200A
-const sp8 = " " as Uni      // U+2002           EN SPACE
+const sp8 = " " as Uni      // U+2002                   EN SPACE
 const sp9 = "  " as Uni     // U+2002 U+200A
-const sp10 = " " as Uni     // U+2008           PUNCTUATION SPACE
+const sp10 = " " as Uni     // U+2008                   PUNCTUATION SPACE
 const sp11 = "  " as Uni    // U+2008 U+200A
-const sp12 = "　" as Uni     // U+3000           IDEOGRAPHIC SPACE
+const sp12 = "　" as Uni     // U+3000                   IDEOGRAPHIC SPACE
 const sp13 = "　 " as Uni    // U+3000 U+200A
-const sp14 = " " as Uni     // U+2001           EM QUAD (not in font yet)
-const sp15 = "  " as Uni    // U+2001 U+200A    (not in font yet)
-const sp16 = " " as Uni     // U+2003           EM SPACE
+const sp14 = "　 " as Uni    // U+3000 U+2009 *
+const sp15 = "　  " as Uni   // U+3000 U+2009 U+200A *
+const sp16 = " " as Uni     // U+2003                   EM SPACE
+
+// * U+2001 EM QUAD, our desired sp14, is not in the font yet. Once it is, these should be replaced.
 
 const SPACES: Partial<Record<Code, Uni>> = {
     [Code["sp1"]]: sp1,
