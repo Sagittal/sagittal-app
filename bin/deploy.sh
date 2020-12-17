8b46db7641cc69bfca80130d29c33d289c099cab
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-17 14:54:47 -0800

    v1.0.73: use standardize build and deploy patterns

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ npm version patch
 NEW_VERSION=$(< package.json grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[:space:]')
 
 rm -r dist/* > /dev/null 2>&1 || true
-webpack
+npm run build
 pushd dist || exit
   git add .
   git commit -m "${NEW_VERSION}"
