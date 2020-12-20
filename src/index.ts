72a4a5928f454463ff826c50aacc2a4bc7a43eb1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-20 10:04:52 -0800

    to-do

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 import "./staff"
+
+// TODO: GIVE STAFF CODE APP A PATH UNDERNEATH THE ROOT SO IT CAN STAY WHAT IT IS AS THE SAGITTAL APP GROWS
+//  Or, actually, it won't be ready to share out until sagittal.github.io is mapped to sagittal.org?
